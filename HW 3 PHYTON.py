107947c042df8b656f7473164979e41bfcc685aa
KingoCengo <56060386+KingoCengo@users.noreply.github.com>
2019-10-06 15:40:14 +0300

    Add files via upload

---
+++ HW 3 PHYTON.py
@@ -0,0 +1,9 @@
+age = 24
+if age < 1:
+    print ('Anıl is an infant.')
+elif age >= 0 and age < 10:
+    print ('Anıl is a child.')
+elif age >= 10 and age < 19:
+    print ('Anıl is a child and an adolescent.')
+else:
+    print ('Anıl is an adult.')
